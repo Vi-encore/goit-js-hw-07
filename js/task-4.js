f40cc973c23f9fae9199177624bba20230c7416c
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-09-25 18:32:50 +0300

    task-4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = e.target.elements.email;
+  const password = e.target.elements.password;
+
+  if (!email.value.trim() || !password.value.trim())
+    alert("All form fields must be filled in");
+
+  const user = {
+    [email.name]: email.value.trim(),
+    [password.name]: password.value.trim(),
+  };
+
+  console.log(user);
+  e.currentTarget.reset();
+});
