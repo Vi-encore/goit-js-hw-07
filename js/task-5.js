7a5c4b35e24454e0e15657f15d5bb2443fccd3c1
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-09-25 18:44:14 +0300

    task-5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const colorInfo = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", () => {
+  const randomHexColor = getRandomHexColor();
+  colorInfo.innerHTML = randomHexColor;
+  body.style.backgroundColor = randomHexColor;
+});
