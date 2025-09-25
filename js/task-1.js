7685ae64d0bbf2a2dabef1c0e0fb27691c05da21
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-09-25 17:26:11 +0300

    task-2 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,9 @@
 const categories = document.querySelector("#categories");
 console.log(`Number of categories: ${categories.children.length}`);
 
+// Can use here querySelectorAll to select all elements with class 'item'
+// That way I would not need Array.from() 
+
 Array.from(categories.children).forEach((el) => {
   console.log(`Category: ${el.children[0].textContent}`);
   console.log(`Elements: ${el.children[1].children.length}`);
